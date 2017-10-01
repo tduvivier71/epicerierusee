892ea2bcbb5f5ce5b7974aa82b87ea18cb120ba2
thierry <thierry@thierryduvivier.com>
2017-10-01 19:05:18 -0400

    chore: initial commit from @angular/cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule} from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { routing } from './app.routing';
+
+import { AlertComponent } from './shared/component/alert.component';
+import { AuthGuard } from './shared/auth/index';
+
+import {AlertService,
+        AuthenticationService,
+        UserService } from './shared/service/index';
+
+import { RegisterComponent } from './register/index';
+import { LoginComponent } from './login/index';
+import { HomeComponent } from './home/index';
+
 
 @NgModule({
+
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    AlertComponent
   ],
+
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    FormsModule,
+    RouterModule,
+    HttpModule,
+    NgbModule.forRoot(),
+    routing
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService
+
+  ],
+
+  bootstrap: [
+    AppComponent
+  ]
 })
+
 export class AppModule { }
